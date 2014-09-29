bf246d2ea18006fa9247ef9f0f7fbef782144d5c
Vlad Piersec <vlad.piersec@gmail.com>
2014-09-29 07:34:26 +0300

    Refactor whole init config

--- init.el
+++ init.el
@@ -1,7 +1,32 @@
-(setq custom-file "~/.emacs.d/configs/custom.el")
-(load "~/.emacs.d/configs/settings.el")
-(load "~/.emacs.d/configs/programming-languages.el")
-(load "~/.emacs.d/configs/looks.el")
-(load custom-file)
-(load "~/.emacs.d/configs/keys.el")
+;; The time when Emacs starts
+(defvar *start-time* (current-time))
 
+(add-to-list 'load-path "~/.emacs.d/config")
+(add-to-list 'load-path "~/.emacs.d/config/general")
+(add-to-list 'load-path "~/.emacs.d/config/programming")
+(add-to-list 'load-path "~/.emacs.d/config/src")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(setq custom-file "~/.emacs.d/config/custom.el")
+
+;; Load general settings
+(require 'general-settings)
+
+;; Load custom file
+(load custom-file)
+
+;; Load keys settings
+(require 'globalkeys-settings)
+
+;; Load Abbrevs
+(require 'abbrevs)
+
+;; Load Theme
+(load-theme 'liso t)
+;; ugly temporary fix until the problem is resolved
+(load "~/.emacs.d/themes/liso-theme.el")
+
+;; Message loading time
+(autoload 'time-passed-since "functions" nil t)
+(message "\n\nMy .init loaded in %.3f seconds\n" (time-passed-since *start-time*))
+
+(current-local-map)
--- themes/liso-theme.el
+++ themes/liso-theme.el
@@ -5,10 +5,10 @@
 ;; Author: Vlad Piersec <vlad.piersec@gmail.com>
 ;; Keywords: theme, themes
 ;; URL: https://github.com/caisah/liso-theme
-;; Version: 1.0
+;; Version: 2.0
 
 ;;; License:
- 
+
 ;; This is free software; you can redistribute it and/or modify it under
 ;; the terms of the GNU General Public License as published by the Free
 ;; Software Foundation; either version 2, or (at your option) any later
@@ -27,201 +27,174 @@
 (deftheme liso
   "Liso - Eclectic Dark Theme inspired by TangoTango")
 
-(custom-theme-set-faces
- 'liso
- '(default ((t (:family "Ubuntu Mono"
-		:foundry "unknown"
-		:width normal
-		:height 128
-		:weight normal
-		:slant normal
-		:underline nil
-		:overline nil
-		:strike-through nil
-		:box nil
-		:inverse-video nil
-		:foreground "#EDEDE1"
-		:background "#272C2E"
-		:stipple nil
-		:inherit nil))))
+(let ((background "#272C2E")
+      (foreground "#EDEDE1") 
+      (cursor-yellow "#E6CB00")
+      (foreground-black "#1A1A1A")
+      (escape "#885D79")
+      (prompt-green "#C8FF03")
+      (highlight-yellow "#ECE185")
+      (region-background "#31454F")
+      (fail-dark-red "#8B0000")
+      (error-red "#E02831")
+      (warning "#FF7E00")
+      (search-pink "#FFEDE6")
+      (search-orange "#CE5C00")
+      (search-brown "#795317") 
+      (comment "#5D6A70")
+      (liso-yellow "#FFE203")
+      (liso-orange "#FF9326")
+      (liso-pink "#FFABAB")
+      (liso-purple "#B46DCC")
+      (liso-red "#C04040")
+      (liso-green "#C8FF03")
+      (liso-dark-green "#6E8C02")
+      (liso-blue "#99D6FF")
+      (link-blue "#7CA4CF")
+      (link-dark-blue "#5075A4")
+      (ml-black "#131617")
+      (ml-yellow "#EAB700")
+      (ml-grey "#C7C7AB")
+      (ml-grey-darker "#414B4E")
+      (paren-blue "#7AD9FF")
+      (paren-red "#9D005C"))
 
- '(cursor ((t (:foreground "#1A1A1A"
-	       :background "#E6CB00"))))
- 
- '(fixed-pitch ((t (:family "Monospace"
-		    :inherit (default)))))
- 
- '(variable-pitch ((t (:family "Sans Serif"))))
- 
- '(escape-glyph ((t (:foreground "#7D5559"))))
- 
- '(minibuffer-prompt ((t (:weight normal
-			  :foreground "#C8FF03"))))
+  (custom-theme-set-faces
+   'liso
+   
+   `(default ((t (:family "Ubuntu Mono"
+                  :foundry "unknown"
+                  :width normal
+                  :height 128
+                  :weight normal
+                  :slant normal
+                  :underline nil
+                  :overline nil
+                  :strike-through nil
+                  :box nil
+                  :inverse-video nil
+                  :foreground ,foreground 
+                  :background ,background
+                  :stipple nil
+                  :inherit nil))))
+   `(cursor ((t (:foreground ,foreground-black :background ,cursor-yellow)))) 
+   `(fixed-pitch ((t (:inherit (default))))) 
+   `(variable-pitch ((t (:family "Sans Serif")))) 
+   `(escape-glyph ((t (:foreground ,escape)))) 
+   `(minibuffer-prompt ((t (:weight normal :foreground ,prompt-green)))) 
+   `(highlight ((t (:foreground ,foreground-black :background ,highlight-yellow)))) 
+   `(region ((t (:background ,region-background)))) 
+   `(shadow ((((class color grayscale) (min-colors 88) (background light)) (:foreground "grey50"))
+             (((class color grayscale) (min-colors 88) (background dark)) (:foreground "grey70"))
+             (((class color) (min-colors 8) (background light)) (:foreground "green"))
+             (((class color) (min-colors 8) (background dark)) (:foreground "yellow"))))
+   `(secondary-selection ((t (:background ,search-brown))))
+   `(trailing-whitespace ((t (:background ,fail-dark-red))))
+   `(whitespace-trailing ((t (:background ,fail-dark-red :foreground ,foreground-black))))
+   `(font-lock-builtin-face ((t (:foreground ,liso-purple))))
+   `(font-lock-comment-face ((t (:foreground ,comment))))
+   `(font-lock-comment-delimiter-face ((t (:inherit (font-lock-comment-face)))))
+   `(font-lock-constant-face ((t (:foreground ,liso-red))))
+   `(font-lock-doc-face ((t (:foreground ,liso-dark-green))))
+   `(font-lock-function-name-face ((t (:foreground ,liso-yellow))))
+   `(font-lock-keyword-face ((t (:foreground ,liso-green))))
+   `(font-lock-negation-char-face ((t nil))) 
+   `(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face))))) 
+   `(font-lock-regexp-grouping-backslash ((t (:inherit (bold))))) 
+   `(font-lock-regexp-grouping-construct ((t (:inherit (bold)))))
+   `(font-lock-string-face ((t (:foreground ,liso-pink))))
+   `(font-lock-type-face ((t (:foreground ,liso-blue))))
+   `(font-lock-variable-name-face ((t (:foreground ,liso-orange))))
+   `(font-lock-warning-face ((t (:weight bold :foreground ,warning :inherit (error)))))
+   `(button ((t (:inherit (link)))))
+   `(link ((t (:underline (:color foreground-color :style line) :foreground ,link-blue)))) 
+   `(link-visited ((t (:underline (:color foreground-color :style line) :foreground ,link-dark-blue :inherit (link)))))
+   `(fringe ((t (:background ,background))))
+   `(header-line ((t (:foreground ,comment :weight bold)))) 
+   `(tooltip ((t (:foreground "black" :background "lightyellow" :inherit (quote variable-pitch)))))
+   `(mode-line ((t (:box (:line-width -1 :color nil :style pressed-button) :foreground "white smoke" :background ,ml-black)))) 
+   `(mode-line-buffer-id ((t (:weight bold :foreground ,ml-yellow)))) 
+   `(mode-line-emphasis ((t (:weight bold)))) 
+   `(mode-line-highlight ((((class color) (min-colors 88)) (:box (:line-width 2 :color "grey40" :style released-button))) (t (:inherit (highlight))))) 
+   `(mode-line-inactive ((t (:weight light :box (:line-width -1 :color nil :style released-button) :foreground ,ml-grey :background ,ml-grey-darker :inherit (mode-line))))) 
+   `(isearch ((t (:foreground ,search-pink :background ,search-orange :underline (:color foreground-color :style line)))))
+   `(isearch-fail ((((class color) (min-colors 88) (background light)) (:background "RosyBrown1"))
+                   (((class color) (min-colors 88) (background dark)) (:background "red4"))
+                   (((class color) (min-colors 16)) (:background "red"))
+                   (((class color) (min-colors 8)) (:background "red"))
+                   (((class color grayscale)) (:foreground "grey"))
+                   (t (:inverse-video t)))) 
+   `(anzu-mode-line ((t (:foreground "PaleVioletRed2" :weight bold))))
+   `(lazy-highlight ((t (:foreground ,foreground-black :background ,search-brown)))) 
+   `(match ((t (:weight bold :foreground ,search-orange :background ,foreground-black))))
+   `(next-error ((t (:inherit (region))))) 
+   `(query-replace ((t (:inherit (isearch))))) 
+   `(show-paren-match ((t (:background ,paren-blue :foreground ,background))))
+   `(show-paren-mismatch ((t (:background ,paren-red :foreground "red"))))
+   `(linum ((t (:foreground "#6F8085" :weight light :height 0.9))))
+   `(vertical-border ((t (:foreground ,ml-black)))) 
+   `(error ((t (:foreground ,error-red :weight semi-bold)))) 
+   `(completions-first-difference ((t (:inherit (highlight))))) 
+   `(diredp-dir-priv ((t (:foreground ,liso-yellow :weight bold))))
+   `(diredp-file-name ((t (:foreground ,foreground :weight normal))))
+   `(diredp-file-suffix ((t (:foreground ,comment :slant italic))))
+   `(diredp-dir-heading ((t (:background ,background :foreground ,ml-yellow :weight ultra-bold))))
+   `(diredp-symlink ((t (:foreground ,link-blue))))
+   `(diredp-compressed-file-suffix ((t (:foreground ,foreground))))
+   `(diredp-flag-mark ((t (:background ,liso-yellow :foreground ,background))))
+   `(diredp-flag-mark-line ((t (:inherit (diredp-flag-mark)))))
+   `(diredp-ignored-file-name ((t (:foreground ,comment))))
+   `(diredp-deletion-file-name ((t (:background ,liso-red :foreground ,foreground))))
+   `(diredp-deletion ((t (:inherit (diredp-deletion-file-name)))))
+   `(diredp-read-priv ((t (:background ,background :foreground ,foreground :weight bold))))
+   `(diredp-write-priv ((t (:background ,background :foreground ,liso-dark-green :weight bold))))
+   `(diredp-exec-priv ((t (:background ,background :foreground ,liso-green :weight bold))))
+   `(diredp-link-priv ((t (:background ,background :foreground ,liso-orange :weight bold))))
+   `(diredp-date-time ((t (:foreground ,comment))))
+   `(diredp-number ((t (:foreground ,comment))))
+   `(diredp-no-priv ((t (:foreground ,foreground))))
+   
+   `(helm-action ((t (:foreground ,foreground :underline nil))))
+   `(helm-selection ((t (:foreground ,paren-red :background ,highlight-yellow :weight bold))))
+   `(helm-source-header ((t (:background: ,background :foreground ,ml-yellow :family "Ubuntu Mono" :weight normal :height: 1.1))))
+   `(helm-visible-mark ((t (:inherit (diredp-flag-mark)))))
+   `(helm-candidate-number ((t (:inherit (match)))))
+   `(helm-buffer-directory ((t (:inherit (diredp-dir-priv)))))
+   `(helm-buffer-size ((t (:foreground ,comment))))
+   `(helm-buffer-process ((t (:inherit (font-lock-doc-face)))))
+   `(helm-ff-directory ((t (:inherit (diredp-dir-priv)))))
+   `(helm-ff-file ((t (:inherit (diredp-file-name)))))
+   `(helm-ff-symlink ((t (:inherit (diredp-symlink)))))
+   `(helm-M-x-key ((t (:foreground ,liso-red :weight bold))))
+   `(helm-match ((t (:foreground ,liso-green))))
+   `(helm-separator ((t (:foreground ,liso-dark-green))))
 
- '(highlight ((t (:foreground "#272C2E"
-		  :background "#E5D86D"))))
- 
- '(region ((t (:background "#1F454D"))))
- 
- '(shadow ((((class color grayscale) (min-colors 88) (background light)) (:foreground "grey50"))
-	   (((class color grayscale) (min-colors 88) (background dark)) (:foreground "grey70"))
-	   (((class color) (min-colors 8) (background light)) (:foreground "green"))
-	   (((class color) (min-colors 8) (background dark)) (:foreground "yellow"))))
- 
- '(secondary-selection ((t (:background "#204A87"))))
- 
- '(trailing-whitespace ((t (:background "#A40000"))))
- 
- '(font-lock-builtin-face ((t (:foreground "#9E60B3"))))
- 
- '(font-lock-comment-delimiter-face ((t (:inherit (font-lock-comment-face)))))
- 
- '(font-lock-comment-face ((t (:foreground "#5D6A70"))))
- 
- '(font-lock-constant-face ((t (:foreground "#FFC2F8"))))
- 
- '(font-lock-doc-face ((t (:foreground "#537A30"
-		           :inherit (font-lock-string-face)))))
- 
- '(font-lock-function-name-face ((t (:foreground "#FFE203"))))
- 
- '(font-lock-keyword-face ((t (:foreground "#C8FF03"))))
- 
- '(font-lock-negation-char-face ((t nil)))
- 
- '(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face)))))
- 
- '(font-lock-regexp-grouping-backslash ((t (:inherit (bold)))))
- 
- '(font-lock-regexp-grouping-construct ((t (:inherit (bold)))))
- 
- '(font-lock-string-face ((t (:foreground "#C9A296"))))
- 
- '(font-lock-type-face ((t (:foreground "#99D6FF"))))
- 
- '(font-lock-variable-name-face ((t (:foreground "#FFA826"))))
- 
- '(font-lock-warning-face ((t (:weight bold
-			       :foreground "#F57900"
-			       :inherit (error)))))
- 
- '(button ((t (:inherit (link)))))
- 
- '(link ((t (:underline (:color foreground-color
-			 :style line)
-	     :foreground "#729FCF"))))
- 
- '(link-visited ((t (:underline (:color foreground-color
-				 :style line)
-		     :foreground "#3465A4"
-		     :inherit (link)))))
- 
- '(fringe ((t (:background "#272C2E"))))
- 
- '(header-line ((t (:foreground "#E6A8DF"
-		    :weight ultra-bold))))
- 
- '(tooltip ((t (:foreground "black"
-	        :background "lightyellow"
-		:inherit (quote variable-pitch)))))
- 
- '(mode-line ((t (:box (:line-width -1 :color nil :style pressed-button)
-		  :foreground "white smoke"
-		  :background "#131617"))))
- 
- '(mode-line-buffer-id ((t (:weight bold
-			    :foreground "#EAB700"))))
- 
- '(mode-line-emphasis ((t (:weight bold))))
- 
- '(mode-line-highlight ((((class color) (min-colors 88)) (:box (:line-width 2
-								:color "grey40"
-								:style released-button)))
-			(t (:inherit (highlight)))))
- 
- '(mode-line-inactive ((t (:weight light
-		           :box (:line-width -1 :color nil :style released-button)
-			   :foreground "#C7C7AB"
-			   :background "#414B4E"
-			   :inherit (mode-line)))))
- 
- '(isearch ((t (:foreground "#FFEDE6"
-		:background "#CE5C00"
-		:underline (:color foreground-color
-				 :style line)))))
- 
- '(isearch-fail ((((class color) (min-colors 88) (background light)) (:background "RosyBrown1"))
-		 (((class color) (min-colors 88) (background dark)) (:background "red4"))
-		 (((class color) (min-colors 16)) (:background "red"))
-		 (((class color) (min-colors 8)) (:background "red"))
-		 (((class color grayscale)) (:foreground "grey"))
-		 (t (:inverse-video t))))
- 
- '(lazy-highlight ((t (:foreground "#231806"
-				   :background "#795317"))))
- 
- '(match ((t (:weight bold
-	      :foreground "#2E3436"
-	      :background "#E9B96E"))))
- 
- '(next-error ((t (:inherit (region)))))
- 
- '(query-replace ((t (:inherit (isearch)))))
+   )
 
- '(show-paren-match ((t (:background "#7AD9FF"
-			 :foreground "#272C2E"))))
- 
- '(show-paren-mismatch ((t (:background "#9D005C"
-			    :foreground "red"))))
+  )
 
- '(dired-directory ((t (:foreground "#FFE203"
-			:weight bold))))
- 
- '(dired-header ((t (:foreground "#E6A8DF"
-		     :background "#3E2C2E"
-		     :weight ultra-bold))))
+;; Ibuffer
+(setq ibuffer-marked-face 'diredp-flag-mark)
+(setq ibuffer-deletion-face 'diredp-deletion-file-name)
 
- '(dired-symlink ((t (:foreground "#FFA8FF"))))
- 
- '(dired-marked ((t (:foreground "#131617"
-		     :background "#A8B3FF"))))
+;;; Other cusomizations
+;; Disable menu bar mode
+(menu-bar-mode -1)
 
- '(dired-flagged ((t (:foreground "#131617"
-		      :background "#D9A3C7"))))
-  
- '(dired-mark ((t (:foreground "#FFE203"))))
+;; Disable toolbar
+(tool-bar-mode -1)
 
- '(dired-ignored ((t (:foreground "#5D6A70"))))
+;; Disable scroll
+(set-scroll-bar-mode nil)
 
- '(dired-perm-write ((t (:foreground "#E6A8DF"
-		         :weight ultra-bold))))
- 
- '(linum ((t (:foreground "#6F8085"
-	      :weight light
-	      :height 0.9))))
+;; Fringes
+(set-fringe-mode '(1 . 1))
 
- '(vertical-border ((t (:foreground "#131617"))))
+;; Show size of file
+(size-indication-mode t)
 
- '(error ((t (:foreground "#E02831"
-	       :weight semi-bold))))
- 
+;; Show column number
+(column-number-mode t)
 
- '(completions-first-difference ((t (:foreground "#FFA826"
-				     :weight bold
-				     :underline t)))))
 
-;; Shell colors
-(setq ansi-color-names-vector ["#131617" "#E02831" "#A8B3FF" "#FFA826" 
-			       "#FFE203" "#99D6FF" "#FFC2F8" "#EDEDE1"])
-
-;; Autoload for MELPA
-
-;;;###autoload
-(and load-file-name
-  (boundp 'custom-theme-load-path)
-  (add-to-list 'custom-theme-load-path
-               (file-name-as-directory (file-name-directory load-file-name))))
- 
 (provide-theme 'liso)
-
-;;; liso-theme.el ends here
