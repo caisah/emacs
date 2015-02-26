941b9924bcf89c3f69686308057c07e80e2bde6b
Vlad Piersec <vlad.piersec@gmail.com>
2015-02-26 21:10:37 +0200

    Eliminate some warnings

--- config/emacs-custom.el
+++ config/emacs-custom.el
@@ -9,10 +9,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(paradox-github-token t))
+ '(paradox-github-token t)
+ '(safe-local-variable-values
+   (quote
+    ((eval when
+           (require
+            (quote rainbow-mode)
+            nil t)
+           (rainbow-mode 1))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ )
--- config/othermodes-settings.el
+++ config/othermodes-settings.el
@@ -71,16 +71,13 @@
 
 ;; Ibuffer
 (defadvice ibuffer-update-title-and-summary (after remove-column-titles)
-  (save-excursion
-    (set-buffer "*Ibuffer*")
-    (toggle-read-only 0)
+  (with-buffer "*Ibuffer*"
     (goto-char 1)
     (search-forward "-\n" nil t)
     (delete-region 1 (point))
     (let ((window-min-height 1))
       ;; save a little screen estate
-      (shrink-window-if-larger-than-buffer))
-    (toggle-read-only)))
+      (shrink-window-if-larger-than-buffer))))
 (ad-activate 'ibuffer-update-title-and-summary)
 
 
