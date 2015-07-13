81e95f1982f89da4d5e5fb9081a17352b550c486
Vlad Piersec <vlad.piersec@gmail.com>
2015-07-13 16:10:25 +0300

    Remove deprecated magit function

--- config/magit-settings.el
+++ config/magit-settings.el
@@ -11,20 +11,5 @@
 (set-default 'magit-unstage-all-confirm nil)
 (setq magit-auto-revert-mode nil)
 
-(defun magit-toggle-all ()
-  "Magit toggle all sections."
-  (interactive)
-  (defun toggle-until-end ()
-    (magit-toggle-section)
-    (if (magit-goto-next-section)
-        (prog1
-          (beginning-of-buffer)
-          (message "Toggled all"))
-      (toggle-until-end)))
-  (beginning-of-buffer)
-  (toggle-until-end))
-(define-key  magit-mode-map (kbd "T") 'magit-toggle-all)
-
-
 (provide 'magit-settings)
 ;;; magit-settings.el ends here
