1175fdd43c01e6ec2127c188fc00ca3ac3929cd1
Vlad Piersec <vlad.piersec@8x8.com>
2016-09-28 08:27:22 +0300

    Change loading order

--- init.el
+++ init.el
@@ -1,16 +1,16 @@
 ;; The time when Emacs starts
 (defvar *start-time* (current-time))
 
+;; Add theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
 ;; Add config and custom packages
 (add-to-list 'load-path "~/.emacs.d/config")
 (add-to-list 'load-path "~/.emacs.d/config/programming")
 (add-to-list 'load-path "~/.emacs.d/custom-packages/")
 
-;; Load general settings
-(require 'general-settings)
-
-;; Add theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-
 ;; Load Theme
 (load-theme 'paleolithic t)
+
+;; Load general settings
+(require 'general-settings)
