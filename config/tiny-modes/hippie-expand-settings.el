ae19df07bbaf36fe643a2337e8dfa5ad72998a03
caisah <vlad.piersec@protonmail.com>
2017-05-01 11:48:21 +0300

    Modify hippies expand

--- config/tiny-modes/hippie-expand-settings.el
+++ config/tiny-modes/hippie-expand-settings.el
@@ -5,7 +5,17 @@
 
 ;;; Code:
 (global-set-key (kbd "M-/") 'hippie-expand)
-
+(setq hippie-expand-try-functions-list
+          '(try-expand-dabbrev
+            try-expand-dabbrev-all-buffers
+            try-expand-dabbrev-from-kill
+            try-complete-file-name-partially
+            try-complete-file-name
+            try-expand-all-abbrevs
+            try-expand-list
+            try-expand-line
+            try-complete-lisp-symbol-partially
+            try-complete-lisp-symbol))
 
 (provide 'hippie-expand-settings)
 ;;; hippie-expand-settings.el ends here
