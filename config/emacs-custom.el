641cfac607eef060fdcb60cf848453d5525e8411
caisah <vlad.piersec@protonmail.com>
2017-11-13 23:03:59 +0200

    Remove rss feeds

--- config/emacs-custom.el
+++ config/emacs-custom.el
@@ -11,6 +11,7 @@
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-tags-on-save t)
  '(package-selected-packages
    (quote
     (hindent nov paradox wttrin password-generator prettier-js list-environment editorconfig cargo skewer-mode rjsx-mode rust-playground toml-mode toml helm-company flycheck-rust rust-mode ace-window go-mode discover-my-major company-quickhelp helm-descbinds helm-describe-modes yasnippet yaml-mode web-mode undo-tree twittering-mode tuareg swiper solarized-theme smartparens smart-mode-line smart-compile slime simple-httpd scss-mode restclient rainbow-mode rainbow-delimiters queue pyvenv pretty-symbols pcache pandoc-mode pallet page-break-lines nginx-mode multiple-cursors markdown-mode magit logito list-processes+ less-css-mode keyfreq json-mode js2-mode inf-mongo imenu-anywhere htmlize highlight-indentation hexrgb helm-swoop helm-projectile helm-package helm-flycheck helm-ag haskell-mode google-translate flycheck-ocaml flycheck-elm find-file-in-project expand-region exec-path-from-shell elm-mode elfeed dired+ deferred company-tern browse-kill-ring anzu)))
