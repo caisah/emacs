35bf4be11a3f3a4e0af360f91805c782e73d7d17
caisah <vlad.piersec@protonmail.com>
2018-12-03 19:48:13 +0200

    Add js2-mode as default for js

--- config/programming-modes/javascript-config.el
+++ config/programming-modes/javascript-config.el
@@ -7,6 +7,7 @@
 ;;; add hippie expand `hippie-expand-try-functions-list
 
 ;;; Code:
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
@@ -56,17 +57,16 @@
     (local-set-key  (kbd "M-.") 'xref-find-definitions)
     ))
 
-(add-hook 'js-mode-hook 'my-jshook)
-(add-hook 'js-mode-hook 'whitespace-mode)
-(add-hook 'js-mode-hook 'company-mode)
-(add-hook 'js-mode-hook 'smartparens-strict-mode)
-(add-hook 'js-mode-hook 'js2-imenu-extras-mode)
-(add-hook 'js-mode-hook 'hs-minor-mode)
-(add-hook 'js-mode-hook 'flycheck-mode)
-(add-hook 'js-mode-hook 'linum-mode)
-(add-hook 'js-mode-hook 'prettier-js-mode)
-(add-hook 'js-mode-hook 'lsp-javascript-typescript-enable)
-(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js2-mode-hook 'my-jshook)
+(add-hook 'js2-mode-hook 'whitespace-mode)
+(add-hook 'js2-mode-hook 'company-mode)
+(add-hook 'js2-mode-hook 'smartparens-strict-mode)
+(add-hook 'js2-mode-hook 'js2-imenu-extras-mode)
+(add-hook 'js2-mode-hook 'hs-minor-mode)
+(add-hook 'js2-mode-hook 'flycheck-mode)
+(add-hook 'js2-mode-hook 'linum-mode)
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'js2-mode-hook 'lsp-javascript-typescript-enable)
 
 (with-eval-after-load 'js2-mode
   (progn
