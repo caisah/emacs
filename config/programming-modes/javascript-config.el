8bfb27b1330b11017fabf816f1116f5eb7eacb1b
caisah <vlad.piersec@protonmail.com>
2018-12-17 21:24:46 +0200

    Use js2-mode for jsx

--- config/programming-modes/javascript-config.el
+++ config/programming-modes/javascript-config.el
@@ -8,7 +8,7 @@
 
 ;;; Code:
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 
