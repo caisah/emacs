725b4abd6fafe7a68df30b63ab70e4f1dd3b9f20
caisah <vlad.piersec@protonmail.com>
2019-01-09 21:32:06 +0200

    Add eww settings

--- config/general-modes/eww-settings.el
+++ config/general-modes/eww-settings.el
@@ -5,16 +5,24 @@
 ;;; Code:
 (require 'eww)
 
-(setq-default eww-search-prefix "https://google.com/search?q=")
+(setq-default eww-search-prefix "https://duckduckgo.com/lite?q=")
 (setq-default shr-color-visible-luminance-min 77)
 
 (define-key eww-mode-map (kbd "n") 'shr-next-link)
 (define-key eww-mode-map (kbd "p") 'shr-previous-link)
 
-(defun eww-open-link-at-point ()
+(defun my-eww-open-link-at-point ()
   "Open link at cursor point in eww."
   (interactive)
   (eww (thing-at-point 'url)))
+
+(defun my-eww-search-buffer ()
+  "Start eww and add the keywords to the buffer name."
+  (interactive)
+  (let ((input (read-from-minibuffer "Keyword: ")))
+    (progn
+        (eww input)
+        (rename-buffer (concat "*eww " input "*")))))
 
 ;; Export:
 (provide 'eww-settings)
