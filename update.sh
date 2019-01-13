cd31866c9e4e925734d9b0e234f5af4a92e045b5
caisah <vlad.piersec@protonmail.com>
2019-01-13 15:28:37 +0200

    Remove ; from sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 echo "Cleaning..."
-cd ~/.emacs.d;
+cd ~/.emacs.d
 echo "Crisp!"
-rm -rf .cask;
-cask update;
-cask upgrade;
-cask;
-echo "Cask is out!";
+rm -rf .cask
+cask update
+cask upgrade
+cask
+echo "Cask is out!"
