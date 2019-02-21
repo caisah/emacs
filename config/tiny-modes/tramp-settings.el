cc7fb5f9016fee32182d07e9b50f8424bbc972ac
caisah <vlad.piersec@protonmail.com>
2019-02-21 21:03:35 +0200

    Add tramp files defaults

--- config/tiny-modes/tramp-settings.el
+++ config/tiny-modes/tramp-settings.el
@@ -4,14 +4,19 @@
 ;;  Tramp config
 
 ;;; Code:
+(setq-default tramp-auto-save-directory "~/.emacs.d/cache"
+              tramp-backup-directory-alist "~/.emacs.d/cache"
+              tramp-persistency-file-name "tramp-persistency.el"
+              tramp-default-method "ssh")
+
+;; easily sudo with /sudo:root@remote-host:<path-to-file>
+(add-to-list 'tramp-default-proxies-alist '(nil "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist '((regexp-quote (system-name)) nil nil))
+
 (with-eval-after-load 'tramp
   (progn
-    (message "tramp loaded")
+    (message "tramp loaded")))
 
-    (setq-default  tramp-default-method "ssh")
-    ;; easily sudo with /sudo:root@remote-host:<path-to-file>
-    (add-to-list 'tramp-default-proxies-alist '(nil "\\`root\\'" "/ssh:%h:"))
-    (add-to-list 'tramp-default-proxies-alist '((regexp-quote (system-name)) nil nil))))
 
 
 (provide 'tramp-settings)
