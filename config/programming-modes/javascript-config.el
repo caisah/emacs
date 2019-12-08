e2dc68744b02cea6042ddf48f516d9f3c429f4d8
caisah <vlad.piersec@protonmail.com>
2019-12-08 16:55:05 +0200

    Rework js config file -> dump js2-mode in favor of js-mode

--- config/programming-modes/javascript-config.el
+++ config/programming-modes/javascript-config.el
@@ -7,9 +7,9 @@
 ;;; add hippie expand `hippie-expand-try-functions-list
 
 ;;; Code:
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js-mode))
 
 
 (defun my-use-eslint-from-node-modules ()
@@ -40,60 +40,51 @@ Return t on success, nil on failure."
   (let ((completion-ignore-case t))
     (all-completions (company-grab-symbol) candidates)))
 
-(defun my-jshook ()
-  "Personal hook for js2-mode."
+(defun my-js-specific ()
+  "Personal hook for \"js-mode\" major mode."
+  (if (not (derived-mode-p 'javascript-mode))
+    (progn
+      ;; Enable autocomplete
+      (company-mode 1)
+      ;; Enable auto formatting
+      (prettier-js-mode 1)
+      ;; Enable yasnippet
+      (yas-minor-mode 1)
+      ;; Enable code navigation
+      (lsp t)
+      ;; Don't consider camelcased full words
+      (subword-mode 1)
+      ;; Don't consider underscored full words
+      (superword-mode 1)
+      ;; Use editor config
+      (editorconfig-mode 1)
+
+      (setq-local company-backends
+                  '(company-lsp
+                    company-dabbrev-code
+                    company-dabbrev
+                    company-files
+                    company-keywords))
+
+      ;; Change mode name to JS
+      (setq mode-name "JS")
+      (unless (my-enable-standard-if-possible)
+        (my-use-eslint-from-node-modules))
+
+      ;; for lsp-javascript
+      (make-local-variable 'company-transformers)
+      (push 'my-company-transformer company-transformers)
+
+      (local-set-key (kbd "C-c C-f") 'hs-toggle-hiding)
+      (local-set-key  (kbd "M-.") 'xref-find-definitions)
+      (local-set-key  (kbd "C-,") 'lsp-find-references)
+      )))
+
+(with-eval-after-load 'js
   (progn
-    (setq-local company-backends
-                '(company-lsp
-                  company-dabbrev-code
-                  company-dabbrev
-                  company-files
-                  company-keywords))
-    ;; Enable yasnippet
-    (yas-minor-mode 1)
-    ;; Change mode name to JS2
-    (setq mode-name "JS2")
-    (unless (my-enable-standard-if-possible)
-      (my-use-eslint-from-node-modules))
-
-    ;; Don't consider camelcased full words
-    (subword-mode 1)
-    ;; Don't consider underscored full words
-    (superword-mode 1)
-    ;; Use editor config
-    (editorconfig-mode 1)
-    ;; for lsp-javascript
-    (make-local-variable 'company-transformers)
-    (push 'my-company-transformer company-transformers)
-
-    (local-set-key (kbd "C-c C-f") 'hs-toggle-hiding)
-    (local-set-key  (kbd "M-.") 'xref-find-definitions)
-    ))
-
-(add-hook 'js2-mode-hook 'my-jshook)
-(add-hook 'js2-mode-hook 'whitespace-mode)
-(add-hook 'js2-mode-hook 'company-mode)
-(add-hook 'js2-mode-hook 'smartparens-strict-mode)
-(add-hook 'js2-mode-hook 'js2-imenu-extras-mode)
-(add-hook 'js2-mode-hook 'hs-minor-mode)
-(add-hook 'js2-mode-hook 'flycheck-mode)
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'js2-mode-hook 'lsp)
-
-(with-eval-after-load 'js2-mode
-  (progn
-    (message "js2-mode loaded")
+    (message "My init :: js-mode loaded")
 
     (setq-default
-     js2-mode-show-parse-errors nil
-     js2-mode-show-strict-warnings nil
-     js2-strict-var-redeclaration-warning nil
-     js2-strict-missing-semi-warning nil
-     js2-mode-show-strict-warnings nil
-     js2-include-browser-externs t
-     js2-include-node-externs t
-     js2-basic-offset 2
-
      ;; Don't use flymake with lsp
      lsp-prefer-flymake :none
 )))
@@ -101,7 +92,7 @@ Return t on success, nil on failure."
 ;; Set config for prettier code formatter
 (with-eval-after-load 'prettier-js
   (progn
-    (message "prettier js loaded")
+    (message "My init :: prettier js loaded")
 
     (setq-default prettier-js-args '("--trailing-comma" "all"
                                      "--bracket-spacing" "true"
@@ -109,6 +100,12 @@ Return t on success, nil on failure."
 
 ;; Use google-chrome for indium
 (setq-default indium-chrome-executable "chromium-browser")
+
+(add-hook 'js-mode-hook 'my-js-specific)
+(add-hook 'js-mode-hook 'whitespace-mode)
+(add-hook 'js-mode-hook 'smartparens-strict-mode)
+(add-hook 'js-mode-hook 'hs-minor-mode)
+(add-hook 'js-mode-hook 'flycheck-mode)
 
 ;; Export
 (provide 'javascript-config)
