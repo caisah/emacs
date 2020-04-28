317acef5a7a5c3b4d4d856c311d1aa17f18b7796
caisah <vlad.piersec@protonmail.com>
2020-04-28 20:08:08 +0300

    Change transient level to allow magit signing commits

--- config/general-modes/magit-settings.el
+++ config/general-modes/magit-settings.el
@@ -13,7 +13,8 @@
   (progn
     (message "My init :: magit loaded")
 
-    (setq-default magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
+    (setq-default magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1
+                  transient-default-level 5)
     ;; Unbind C-x M-g as is used for helm-ag
     (define-key magit-file-mode-map (kbd "C-x M-g") nil)))
 
