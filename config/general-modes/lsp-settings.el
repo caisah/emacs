20f1f88306393344ea3c01ed9588e839263941a6
caisah <vlad.piersec@protonmail.com>
2022-01-26 17:05:00 +0200

    Enable lsp diagnostics

--- config/general-modes/lsp-settings.el
+++ config/general-modes/lsp-settings.el
@@ -7,7 +7,6 @@
 
 (defun my-lsp-hook ()
   "Hook used for lsp-mode."
-  (lsp-diagnostics-mode -1)
   (lsp-modeline-diagnostics-mode -1))
 
 (add-hook 'my-lsp-hook 'lsp-mode-hook)
@@ -20,9 +19,7 @@
      lsp-idle-delay 0.500
      lsp-headerline-breadcrumb-enable nil
      lsp-print-performance t
-     lsp-modeline-diagnostics-enable nil
-     lsp-diagnostics-disabled-modes '(js-mode)
-     lsp-diagnostic-package :none)
+     lsp-modeline-diagnostics-enable nil)
     (define-key lsp-mode-map (kbd "s-l i") 'lsp-ivy-workspace-symbol)))
 
 (provide 'lsp-settings)
