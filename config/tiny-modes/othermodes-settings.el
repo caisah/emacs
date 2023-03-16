a1199dd905d70ef0e7994529afd9f9f8157043fa
caisah <vlad.piersec@protonmail.com>
2023-03-16 19:05:56 +0200

    Remove anzu

--- config/tiny-modes/othermodes-settings.el
+++ config/tiny-modes/othermodes-settings.el
@@ -90,10 +90,6 @@
 
 (straight-use-package 'ag)
 
-;; Use anzu
-(straight-use-package 'anzu)
-(global-anzu-mode t)
-
 ;; Show beautified page breaks
 (straight-use-package 'page-break-lines)
 (global-page-break-lines-mode t)
