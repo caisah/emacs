9b76a3560f2ed04e54626d0ee06b385accde89ff
Vlad Piersec <vlad.piersec@protonmail.com>
2025-03-17 14:50:55 +0200

    disable flymake when starting flycheck

--- elisp/my-functions.el
+++ elisp/my-functions.el
@@ -198,6 +198,7 @@
                       (expand-file-name "node_modules/.bin/eslint"
                                         root))))
     (when (and eslint (file-executable-p eslint))
+      (flymake-mode -1)
       (setq-local flycheck-javascript-eslint-executable eslint)
       (setq-default flycheck-disabled-checkers '(javascript-jshint))
       (setq-local flycheck-checker 'javascript-eslint))))
