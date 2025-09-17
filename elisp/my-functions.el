8867ca6002ad6d4861b9d2b0b2a82b67abcbc93b
caisah <vlad.piersec@protonmail.com>
2025-09-17 13:25:38 +0300

    add node version to settings

--- elisp/my-functions.el
+++ elisp/my-functions.el
@@ -183,7 +183,7 @@
   (cond ((deno-project-p) '("deno" "lsp" :initializationOptions '(:enable t :lint t)))
         (t '("typescript-language-server" "--stdio" :initializationOptions
              (:plugins [(:name "ts-lit-plugin"
-                         :location (concat my-base-user-path ".nvm/versions/node/v22.14.0/lib/node_modules/ts-lit-plugin"))])))))
+                         :location (concat my-base-user-path ".nvm/versions/node/" my-node-version "/lib/node_modules/ts-lit-plugin"))])))))
 
 (defun my-use-eslint-from-node-modules ()
   "Use local eslint from node_modules before global."
