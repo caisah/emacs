560059b9b3cce06df6556fb576814af43d6ec096
caisah <vlad.piersec@protonmail.com>
2025-10-03 22:20:15 +0300

    remove yasnippet

--- init.el
+++ init.el
@@ -252,13 +252,6 @@
   (exec-path-from-shell-initialize))
 
 
-(use-package yasnippet
-  :straight t
-
-  :custom
-  (yas-snippet-dirs '("~/.emacs.d/snippets")))
-
-
 (use-package re-builder
   :custom
   (reb-re-syntax 'string))
